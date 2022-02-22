91b506b2d8110cd2a73afe4ab01769dd09c4755e
Onur Ucar <onurucar@iu.edu>
2022-02-21 22:26:19 -0500

    weapon switching

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -5,8 +5,22 @@ export var mouse_sens = 0.5
 onready var camera = $Camera
 onready var character_mover = $CharacterMover
 onready var health_manager = $HealthManager
+onready var weapon_manager = $Camera/WeaponManager
 
 var is_dead = false
+
+var hotkeys = { 
+	KEY_1: 0, 
+	KEY_2: 1, 
+	KEY_3: 2, 
+	KEY_4: 3,
+	KEY_5: 4, 
+	KEY_6: 5, 
+	KEY_7: 6, 
+	KEY_8: 7,
+	KEY_9: 8, 
+	KEY_0: 9, 
+}
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -41,11 +55,24 @@ func _process(_delta):
 
 # Mouse inputs
 func _input(event):
-	# Any mouse related input
+	# Looking around with mouse
 	if event is InputEventMouseMotion:
 		rotation_degrees.y -= mouse_sens * event.relative.x
 		camera.rotation_degrees.x -= mouse_sens * event.relative.y
 		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
+		
+	# Take inputs for the weapons
+	# Switching the weapon with the help of hotkeys
+	if event is InputEventKey and event.pressed:
+		if event.scancode in hotkeys:
+			weapon_manager.switch_to_weapon_slot(hotkeys[event.scancode])
+		# Switching the weapon by scrolling the wheel
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == BUTTON_WHEEL_DOWN:
+			weapon_manager.switch_to_next_weapon()
+		if event.button_index == BUTTON_WHEEL_UP:
+			weapon_manager.switch_to_last_weapon()
+			
 		
 func hurt(damage, direction):
 	health_manager.hurt(damage, direction)
